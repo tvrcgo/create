f3d5ccb61f9d54707f0e8f95ec9a7235767c81d9
tvrcgo <tvrcgo@gmail.com>
2020-10-13 15:57:58 +0800

    upgrade annar@2

--- packages/wx-miniprogram/content/src/app.js
+++ packages/wx-miniprogram/content/src/app.js
@@ -1,9 +1,6 @@
-import React from 'react'
-import 'anna-remax-ui/dist/anna.css'
+import 'annar/dist/anna.css'
 import './app.less'
 
-export default class App extends React.Component {
-  render() {
-    return this.props.children
-  }
+export default (props) => {
+  return props.children
 }
