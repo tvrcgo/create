e32f8509e6c196b3d0b050fc89b003ccd89243fd
tvrcgo <tvrcgo@gmail.com>
2022-09-22 11:00:34 +0800

    taro config

---
+++ packages/wx-miniprogram/content/config/index.js
@@ -0,0 +1,33 @@
+const config = {
+  projectName: '{{name}}',
+  date: '2022-01-01',
+  designWidth: 750,
+  deviceRatio: {
+    640: 2.34 / 2,
+    750: 1,
+    828: 1.81 / 2
+  },
+  sourceRoot: 'src',
+  outputRoot: 'dist',
+  plugins: [],
+  defineConstants: {
+  },
+  copy: {
+    patterns: [
+    ],
+    options: {
+    }
+  },
+  framework: 'react',
+  compiler: 'webpack5',
+  cache: {
+    enable: false // Webpack 持久化缓存配置，建议开启。默认配置请参考：https://docs.taro.zone/docs/config-detail#cache
+  }
+}
+
+module.exports = function (merge) {
+  if (process.env.NODE_ENV === 'development') {
+    return merge({}, config, require('./dev'))
+  }
+  return merge({}, config, require('./prod'))
+}
