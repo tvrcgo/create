cef9be69fa0b6f6ef0104fac9e35747dfb1e5718
tvrcgo <tvrcgo@gmail.com>
2023-09-04 13:26:38 +0800

    page tpl

--- packages/umi/content/src/pages/index.tsx
+++ packages/umi/content/src/pages/index.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Helmet } from 'umi'
 import styles from './index.less'
+import useReducers from '@/hooks/reducers'
 
 export default function () {
+
+  const [state, dispatch] = useReducers({
+
+  }, {})
+
+  useEffect(() => {
+
+  }, [])
+
   return (
     <div className={styles.page}>
+      <Helmet>
+        <title>页面标题</title>
+      </Helmet>
       <h1>Home page</h1>
     </div>
   )
