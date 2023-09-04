7709d514e70714995d370320e0bd9447cac21c24
tvrcgo <tvrcgo@gmail.com>
2023-09-04 13:26:14 +0800

    ios safe-area meta

--- packages/umi/content/src/layouts/index.tsx
+++ packages/umi/content/src/layouts/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
-import { Outlet } from 'umi'
+import { Outlet, Helmet } from 'umi'
 
 export default () => {
   return (
-    <div><Outlet /></div>
+    <div>
+      <Helmet>
+        <meta name='viewport' content='width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, viewport-fit=cover' />
+      </Helmet>
+      <Outlet />
+    </div>
   )
 }
